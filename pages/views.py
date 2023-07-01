9eff9aa1fde648b342cd6c09272c94af3e23acad
Your Name <you@example.com>
2023-07-01 19:16:01 +0200

    add tasks

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TaskForm
+def index(request):
+    data_save=TaskForm()
+    if request.method=='POST':
+        data_save=TaskForm(request.POST,request.FILES)
+        if data_save.is_valid():
+            data_save.save()
+            return redirect('/')
+    context={
+        'tasks':Task.objects.all(),
+        'form':data_save
+    }
+    return render(request,'pages/index.html',context)
